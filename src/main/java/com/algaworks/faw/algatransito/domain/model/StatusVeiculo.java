4c76baa33f127480b8a0da07cb3d403d3ad1ca37
Fernando Alexandre Wahl <wahlfernando@yahoo.com.br>
2026-02-15 16:03:58 -0300

    Criação do enumerador (enum), StatusVeiculo; Criação da Classe Veiculo;

---
+++ src/main/java/com/algaworks/faw/algatransito/domain/model/StatusVeiculo.java
@@ -0,0 +1,6 @@
+package com.algaworks.faw.algatransito.domain.model;
+
+public enum StatusVeiculo {
+
+    REGULAR, APREENDIDO
+}
---
+++ src/main/java/com/algaworks/faw/algatransito/domain/model/Veiculo.java
@@ -0,0 +1,31 @@
+package com.algaworks.faw.algatransito.domain.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @ManyToOne
+    private Proprietario proprietario;
+    private String marca;
+    private String modelo;
+    private String placa;
+
+    @Enumerated(EnumType.STRING)
+    private StatusVeiculo statusVeiculo;
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataApreensao;
+}
